15a2072dad145421400ded394bf3d4fabcc7373d
developer-dmp <developer.dmp@gmail.com>
2021-02-06 16:04:23 -0500

    Added Attraction.kt model based off the raw JSON file of data. Added view_holder_attraction.xml for use in the UI.

---
+++ app/src/main/java/com/dmp/senya/data/Attraction.kt
@@ -0,0 +1,16 @@
+package com.dmp.senya.data
+
+data class Attraction(
+    val description: String = "",
+    val facts: List<String> = listOf(),
+    val id: String = "",
+    val image_url: String = "",
+    val location: Location = Location(),
+    val months_to_visit: String = "",
+    val title: String = ""
+) {
+    data class Location(
+        val latitude: String = "",
+        val longitude: String = ""
+    )
+}
