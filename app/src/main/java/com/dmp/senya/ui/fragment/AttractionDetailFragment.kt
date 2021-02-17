ac95c6b3fe214ac2d5303020456337c60b5c168d
developer-dmp <developer.dmp@gmail.com>
2021-02-17 12:09:33 -0500

    Added our location icon and menu to the app. Updated our AttractionDetailFragment.kt to display the newly created menu and handle the on click of the items we care about.

--- app/src/main/java/com/dmp/senya/ui/fragment/AttractionDetailFragment.kt
+++ app/src/main/java/com/dmp/senya/ui/fragment/AttractionDetailFragment.kt
@@ -1,10 +1,13 @@
 package com.dmp.senya.ui.fragment
 
+import android.content.Intent
+import android.content.res.ColorStateList
+import android.net.Uri
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import androidx.core.content.ContextCompat
 import androidx.navigation.fragment.navArgs
+import com.dmp.senya.R
 import com.dmp.senya.data.Attraction
 import com.dmp.senya.databinding.FragmentActtractionDetailBinding
 import com.squareup.picasso.Picasso
@@ -17,6 +20,11 @@ class AttractionDetailFragment : BaseFragment() {
     private val safeArgs: AttractionDetailFragmentArgs by navArgs()
     private val attraction: Attraction by lazy {
         attractions.find { it.id == safeArgs.attractionId }!!
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
     }
 
     override fun onCreateView(
@@ -39,6 +47,24 @@ class AttractionDetailFragment : BaseFragment() {
         binding.numberOfFactsTextView.text = "${attraction.facts.size} facts"
         binding.numberOfFactsTextView.setOnClickListener {
             // todo
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_attraction_detail, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menuItemLocation -> {
+                val uri = Uri.parse("geo:${attraction.location.latitude},${attraction.location.longitude}?z=9&q=${attraction.title}")
+                val mapIntent = Intent(Intent.ACTION_VIEW, uri)
+                mapIntent.setPackage("com.google.android.apps.maps")
+                startActivity(mapIntent)
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
         }
     }
 
